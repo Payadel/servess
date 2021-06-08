0ae7df4d05a80c371dc76da09a52f2717f645e61
Hamid Molareza <35568753+HamidMolareza@users.noreply.github.com>
2021-06-08 17:37:04 +0430

    Adds sudo to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 output=$1
 slnPath=./Servess/
 
-dotnet publish -c Release -r linux-x64 --self-contained false -o $output $slnPath
+sudo dotnet publish -c Release -r linux-x64 --self-contained false -o $output $slnPath
