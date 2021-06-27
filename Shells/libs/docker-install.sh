61bc5dd793223380196f36c16da1edf0e4d29ec3
Hamid Molareza <kitm.hamid@gmail.com>
2021-06-27 14:27:15 +0430

    Adds docker login in installation

--- Shells/libs/docker-install.sh
+++ Shells/libs/docker-install.sh
@@ -1,18 +1,24 @@
 sudo apt-get update
 
 sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg \
-    lsb-release
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
-    "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
 
 sudo apt-get update
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+if [ $? != 0 ]; then
+  echo "Operation failed."
+  exit 1
+fi
+
+docker login
 sudo docker run hello-world
