79f7e794b6d501f1b36bd602cbae149cecd8ec9c
Hamid Molareza <kitm.hamid@gmail.com>
2021-07-03 02:48:22 +0430

    Updates user-add-sudo.sh

--- Shells/libs/user-add-sudo.sh
+++ Shells/libs/user-add-sudo.sh
@@ -1,5 +1,9 @@
-printf "Username: "
-read username
+if [ -z $1 ]; then
+    printf "Username: "
+    read username
+else
+    username=$1
+fi
 
 /opt/shell-libs/user-add.sh "$username"
 sudo usermod -aG sudo "$username"
