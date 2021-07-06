6611bcf3347455400c60945050c1350c876b4325
Hamid Molareza <kitm.hamid@gmail.com>
2021-07-06 21:20:06 +0430

    Updates welcome.sh

--- Shells/libs/welcome.sh
+++ Shells/libs/welcome.sh
@@ -1,5 +1,7 @@
-for fileName in /etc/update-motd.d/*; do
-    if [ "$fileName" != "/etc/update-motd.d/98-fsck-at-reboot" ] && [ -x "$fileName" ]; then
-        $fileName
-    fi
-done
+# for fileName in /etc/update-motd.d/*; do
+#     if [ "$fileName" != "/etc/update-motd.d/98-fsck-at-reboot" ] && [ -x "$fileName" ]; then
+#         $fileName
+#     fi
+# done
+
+sudo run-parts /etc/update-motd.d/
