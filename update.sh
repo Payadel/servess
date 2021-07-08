7cfd297c079f5d2981664993a4f0cba2e18114dc
Hamid Molareza <kitm.hamid@gmail.com>
2021-07-08 14:24:50 +0430

    Removes cd from update.sh

--- update.sh
+++ update.sh
@@ -24,26 +24,31 @@ if [ -d "$name" ]; then
     printf "Directory $name is exist. do you want replace it? (y/n): "
     read delete
     if [ "$delete" = "y" ] || [ "$delete" = "Y" ]; then
-        sudo rm "$name"
+        sudo rm -r "$name"
+    else
+        random_string=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
+        git_dir="$currentDir/$random_string"
     fi
 fi
 
-git clone https://github.com/HamidMolareza/$name.git
+git_shells_dir="$git_dir/Shells"
+git_servess_dir="$git_dir/$name"
+
+echo "Cloning..."
+git clone https://github.com/HamidMolareza/$name.git "$git_dir"
 if [ $? != 0 ]; then
     echo "Operation failed." >&2
     exit $?
 fi
 
-cd "$name"
 echo "done."
 echo "======================================================================="
 
 echo "Installing shell libs..."
-cd "Shells"
 if [ -d "$libs_dir" ]; then
     sudo rm -r "$libs_dir"
 fi
-sudo mkdir -p "$libs_dir" && sudo cp libs/* "$libs_dir/" && sudo chmod -R 750 "$libs_dir/"
+sudo mkdir -p "$libs_dir" && sudo cp -r "$git_shells_dir/libs/" "$libs_dir/" && sudo chmod -R 750 "$libs_dir"
 if [ $? != 0 ]; then
     echo "Operation failed." >&2
     clear_git "$git_dir"
@@ -54,7 +59,7 @@ fi
 if [ -d "$shells_dir" ]; then
     sudo rm -r "$shells_dir"
 fi
-sudo mkdir -p "$shells_dir" && sudo cp installer.sh "$shells_dir/" && sudo chmod -R 750 "$shells_dir/"
+sudo mkdir -p "$shells_dir" && sudo cp "$git_shells_dir/installer.sh" "$shells_dir/" && sudo chmod -R 750 "$shells_dir/"
 if [ $? != 0 ]; then
     echo "Operation failed." >&2
     clear_git "$git_dir"
@@ -67,7 +72,6 @@ echo "======================================================================="
 
 echo "Installing $cliName..."
 echo "Building project..."
-cd "$git_dir/$name"
 
 if [ -d "$install_servess_dir" ]; then
     sudo rm -r "$install_servess_dir"
@@ -80,7 +84,8 @@ if [ $? != 0 ]; then
     exit $?
 fi
 
-sudo chmod 750 ./publish.sh && (./publish.sh "$install_servess_dir")
+publish_file="$git_servess_dir/publish.sh"
+sudo chmod 750 "$publish_file" && ($publish_file "$install_servess_dir" "$git_servess_dir/$name")
 if [ $? != 0 ]; then
     echo "Operation failed." >&2
     clear_git "$git_dir"
@@ -90,8 +95,8 @@ fi
 
 echo "done."
 
-echo "Adds execute access..."
-cd $install_servess_dir && sudo chmod 750 "$cliName"
+echo "Adding execute access..."
+sudo chmod 750 "$install_servess_dir/$cliName"
 if [ $? != 0 ]; then
     echo "Operation failed." >&2
     clear_git "$git_dir"
@@ -100,9 +105,9 @@ if [ $? != 0 ]; then
 fi
 echo "done."
 
-echo "Adds file to bin dir..."
-if [ -f "$bin_path" ]; then
-    sudo rm "$bin_path"
+echo "Creating ln to $bin_path..."
+if [ -f "$bin_path/$cliName" ]; then
+    sudo rm "$bin_path/$cliName"
 fi
 sudo ln -s "$install_servess_dir/$cliName" "$bin_path"
 if [ $? != 0 ]; then
