3147e44d493dbbd1e7b8a2b8753ce66d14c39a95
Hamid Molareza <kitm.hamid@gmail.com>
2021-07-11 09:35:52 +0430

    Minor updates user-delete.sh

--- Shells/libs/user-delete.sh
+++ Shells/libs/user-delete.sh
@@ -15,12 +15,14 @@ else
     username=$1
 fi
 
+#Check is user exist?
 id "$username" &>/dev/null
 if [ "$?" != "0" ]; then
     echo -e "$ERROR_COLORIZED: The user does not exist." >&2
     exit 1
 fi
 
+#Find user home dir
 homeDir=$(getent passwd "$username" | cut -d: -f6)
 if [ "$?" != 0 ] || [ -z "$homeDir" ]; then
     echo -e "$ERROR_COLORIZED: Can't detect user home directory."
@@ -31,17 +33,18 @@ if [ "$?" != 0 ] || [ -z "$homeDir" ]; then
         echo -e "$ERROR_COLORIZED: Invalid directory."
         exit 1
     fi
-
-    ls -lh "$homeDir"
-    echo -e "$WARNING_COLORIZED: Are you sure? (y/n): "
-    read confirm_user_dir
-    if [ "$confirm_user_dir" != "y" ] && [ "$confirm_user_dir" != "Y" ]; then
-        echo -e "$INFO_COLORIZED: Operation canceled."
-        exit 0
-    fi
+else
+    echo -e "$INFO_COLORIZED: User home directory detected: $homeDir"
 fi
 
-echo -e "$INFO_COLORIZED: User home directory detected: $homeDir"
+ls -lh "$homeDir"
+echo -e "$WARNING_COLORIZED: Are you sure? (y/n): "
+read confirm_user_dir
+if [ "$confirm_user_dir" != "y" ] && [ "$confirm_user_dir" != "Y" ]; then
+    echo -e "$INFO_COLORIZED: Operation canceled."
+    exit 0
+fi
+#============================================================================
 
 #log out active sessions
 user_sessions=$(pgrep -u test)
@@ -69,7 +72,6 @@ if [ "$create_backup" = "y" ] || [ "$create_backup" = "Y" ]; then
     backup_dir="/var/server-backups"
     printf "Backup directory (default: $backup_dir): "
     read input
-
     if [ ! -z "$input" ]; then
         backup_dir="$input"
     fi
@@ -79,7 +81,7 @@ if [ "$create_backup" = "y" ] || [ "$create_backup" = "Y" ]; then
         exit_if_operation_failed "$?"
     fi
 
-    sudo tar -zcvf "$backup_dir/${username}_backup.tgz $homeDir"
+    sudo tar -zcvf "$backup_dir/user_${username}_backup.tgz" "$homeDir"
     exit_if_operation_failed "$?"
 fi
 
