7676ab37df821e6cf4b9e0d1f7536048bdd588eb
Hamid Molareza <kitm.hamid@gmail.com>
2021-07-11 08:10:29 +0430

    Updates user-add.sh

--- Shells/libs/user-add.sh
+++ Shells/libs/user-add.sh
@@ -31,20 +31,27 @@ fi
 #Sudo group?
 printf "Add user to sudo group? (y/n): "
 read sudo_group
-if [ "$sudo_group" == "y" ] || [ "$sudo_group" == "Y" ]; then
+if [ "$sudo_group" -eq "y" ] || [ "$sudo_group" -eq "Y" ]; then
     sudo usermod -aG sudo "$username"
 fi
 
 #root group?
 printf "Add user to root group? (y/n): "
 read root_group
-if [ "$root_group" == "y" ] || [ "$root_group" == "Y" ]; then
+if [ "$root_group" -eq "y" ] || [ "$root_group" -eq "Y" ]; then
     sudo usermod -aG root "$username"
 fi
 
 #Disables welcome banner
 printf "Disables welcome banner? (y/n): "
 read disable_banner
-if [ "$disable_banner" == "y" ] || [ "$disable_banner" == "Y" ]; then
+if [ "$disable_banner" -eq "y" ] || [ "$disable_banner" -eq "Y" ]; then
     sudo touch "$home_dir/.hushlogin" && chattr +i "$home_dir/.hushlogin"
+fi
+
+#Disables welcome banner
+printf "force expire the password (the user must change password after login)? (y/n): "
+read expire_password
+if [ "$expire_password" -eq "y" ] || [ "$expire_password" -eq "Y" ]; then
+    sudo passwd -e "$username"
 fi
