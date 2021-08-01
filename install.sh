968d50f89eb1646ad04d1f20328cc09048e764d6
Hamid Molareza <kitm.hamid@gmail.com>
2021-08-01 16:13:49 +0430

    Updates install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,17 @@
+exit_if_operation_failed() {
+  local code="$1"
+  local message="$2"
+
+  if [ "$code" != "0" ]; then
+    if [ -n "$message" ]; then
+      echo -e "$message" >&2
+    else
+      echo "Operation failed with code $code."
+    fi
+    exit "$code"
+  fi
+}
+
 name=Servess
 cliName=servess
 install_dir="/opt"
@@ -6,75 +20,45 @@ bin_path="/usr/local/bin/servess"
 libs_dir="$install_dir/shell-libs"
 
 echo "Install git..."
-if ! sudo apt install -y git-all; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo apt install -y git-all
+
 
 echo "Cloning project..."
-if ! git clone https://github.com/HamidMolareza/$name.git; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+git clone https://github.com/HamidMolareza/$name.git
+exit_if_operation_failed "$?"
 
 echo "Installing shell libs..."
-if ! sudo mkdir -p "$libs_dir" && sudo cp $name/Shells/libs/* "$libs_dir/" && sudo chmod -R 750 "$libs_dir/"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo mkdir -p "$libs_dir" && sudo cp $name/Shells/libs/* "$libs_dir/" && sudo chmod -R 750 "$libs_dir/"
+exit_if_operation_failed "$?"
 
-if ! sudo cp name/Shells/installer.sh "$libs_dir/" && sudo chmod 750 "$libs_dir/installer.sh"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo cp name/Shells/installer.sh "$libs_dir/" && sudo chmod 750 "$libs_dir/installer.sh"
+exit_if_operation_failed "$?"
 
 #Servess
 
 echo "Install dotnet..."
-if ! sudo "$libs_dir/dotnet5-install.sh"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo "$libs_dir/dotnet5-install.sh"
+exit_if_operation_failed "$?"
 
 echo "Installing $cliName..."
+
 echo "Building project..."
+sudo mkdir -p "$install_servess_dir" && sudo chmod 750 "$install_servess_dir"
+exit_if_operation_failed "$?"
 
-if ! sudo mkdir -p "$install_servess_dir" && sudo chmod 750 "$install_servess_dir"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
-
-if ! sudo chmod 750 $name/$name/publish.sh && ($name/$name/publish.sh "$install_servess_dir"); then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo chmod 750 $name/$name/publish.sh && ($name/$name/publish.sh "$install_servess_dir")
+exit_if_operation_failed "$?"
 
 echo "Adds execute access..."
-if ! cd $install_servess_dir && sudo chmod 750 "$cliName"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+cd $install_servess_dir && sudo chmod 750 "$cliName"
+exit_if_operation_failed "$?"
 
 echo "Adds file to bin dir..."
-if ! sudo ln -s "$install_servess_dir/$cliName" "$bin_path"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo ln -s "$install_servess_dir/$cliName" "$bin_path"
+exit_if_operation_failed "$?"
 
 echo "Clear git source..."
-if ! sudo rm -r "$name/"; then
-  code="$?"
-  echo "Operation failed." >&2
-  exit $code
-fi
+sudo rm -r "$name/"
+exit_if_operation_failed "$?"
 
 echo "done."
