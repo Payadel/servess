cf502cf0d00cfbf8484caec480215312290ad05e
Hamid Molareza <kitm.hamid@gmail.com>
2022-01-21 00:25:47 +0330

    minor update

--- Shells/libs/ssl-check.sh
+++ Shells/libs/ssl-check.sh
@@ -14,6 +14,7 @@ expire_day_in_seconds=$((expire_day * 24 * 3600))
 
 if [ ! -f "$sslPaths_file" ]; then
     echo_error "Can't find ssl paths: $sslPaths_file"
+    echo_info "Input your ssl paths (like fullchain.pem) to $sslPaths_file"
     exit 1
 fi
 
@@ -27,7 +28,7 @@ for line in $(cat $sslPaths_file); do
         continue
     fi
 
-    validation_result=$(openssl x509 -enddate -noout -in $line -checkend $expire_day_in_seconds | grep "Certificate will expire")
+    validation_result=$(openssl x509 -enddate -noout -in "$line" -checkend $expire_day_in_seconds | grep "Certificate will expire")
     if [ ! -z "$validation_result" ]; then
         echo_warning "Your ssl in $line will expires less than $expire_day days."
         is_error=1
