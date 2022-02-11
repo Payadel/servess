6f7138de2bf646b3e438a82fdc2c1d64a9cafd54
Hamid Molareza <kitm.hamid@gmail.com>
2022-02-11 12:55:24 +0330

    Minor update.

--- Servess/publish.sh
+++ Servess/publish.sh
@@ -1,4 +1,4 @@
 output=$1
 slnPath=$2
 
-sudo dotnet publish -c Release -r linux-x64 --self-contained false -o $output $slnPath
+dotnet publish -c Release -r linux-x64 --self-contained false -o $output $slnPath
